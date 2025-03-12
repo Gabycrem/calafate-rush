ce8270d1fc5f40a75fdffe5f1d72f917e448982f
gabycrem <gabymacre@gmail.com>
2025-03-12 15:57:43 -0300

    planta de calafate recibe disparo

--- Assets/Code/CalafatePlants/CalafatePlant.cs
+++ Assets/Code/CalafatePlants/CalafatePlant.cs
@@ -1,4 +1,6 @@
 
+using System.Collections;
+using System.Numerics;
 using UnityEngine;
 
 namespace Calafate
@@ -7,16 +9,52 @@ namespace Calafate
     {
         public GameObject PlantWithFruits;
         public GameObject PlantWithoutFruits;
-        void Start()
+        [SerializeField] private float _regenerationTime = 5f;
+
+
+        // Recibe disparo. Oculta frutos en la planta. 
+        // Próx: hacer aparecer frutos caidos. si el player esta a una cierta distancia 
+        void OnCollisionEnter(Collision collision)
         {
-            PlantWithFruits.SetActive(true);
-            PlantWithoutFruits.SetActive(false);
+            if (collision.gameObject.CompareTag("Projectile"))
+            {
+
+                // Ocultar la planta con frutos y mostrar la sin frutos
+                ChangePlantState(false);
+                Debug.Log("le pego");
+
+                //Soltar los frutos
+
+                //DropFruits();
+
+                // Destruir el proyectil
+                Destroy(collision.gameObject);
+
+                //Iniciar regeneración de frutos
+                StartCoroutine(RegenerateFruits());
+            }
         }
 
-        void Update()
+        // Regeneración de Frutos
+        IEnumerator RegenerateFruits()
         {
-
+            yield return new WaitForSeconds(_regenerationTime);
+            ChangePlantState(true);
         }
+
+        // Función para cambiar el estado de la planta
+        void ChangePlantState(bool hasFruits)
+        {
+            PlantWithFruits.SetActive(hasFruits);
+            PlantWithoutFruits.SetActive(!hasFruits);
+        }
+
+        //Función para soltar los frutos
+        /*
+        void DropFruits()
+        {
+            
+        }
+        */
     }
-
 }
